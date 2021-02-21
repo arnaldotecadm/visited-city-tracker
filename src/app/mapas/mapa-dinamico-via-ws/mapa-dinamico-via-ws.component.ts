f694b0ff1c74ef86aa772550961be97e1a0030f5
Arnaldo C. Bezerra <arnaldotecadm@hotmail.com>
2021-02-20 23:23:50 -0300

    Primeira versao mapa completo from ws

--- src/app/mapas/mapa-dinamico-via-ws/mapa-dinamico-via-ws.component.ts
+++ src/app/mapas/mapa-dinamico-via-ws/mapa-dinamico-via-ws.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { geoMercator, geoPath, json, select } from "d3";
+import * as topojson from "topojson-client";
 
 @Component({
   selector: "app-mapa-dinamico-via-ws",
@@ -15,28 +16,34 @@ export class MapaDinamicoViaWsComponent implements OnInit {
     this.draw(svg);
   }
 
+  //         min-x,   min-y,  width             height
+  viewbox = "-50.9904 -5.2718 30.58069999999999 35.015699999999995";
   draw(svg) {
-    const projection = geoMercator().scale(1000);
+    const projection = geoMercator().scale(40000);
+    projection.reflectY();
     const pathGenerator = geoPath().projection(projection);
 
     json(
-      "https://servicodados.ibge.gov.br/api/v3/malhas/estados/42?formato=application/vnd.geo+json&qualidade=minima&intrarregiao=municipio"
+      "https://servicodados.ibge.gov.br/api/v3/malhas/paises/BR?intrarregiao=regiao&formato=application/json"
     ).then((data: any) => {
       let dados;
       dados = data;
-      //dados = topojson.feature(data, data.objects["UF42MU"]);
+      dados = topojson.feature(data, data.objects["BRGR"]);
       console.log(dados);
+
+      //svg.attr()
 
       const gMap = svg.append("g");
       gMap
+        .attr("transform", "scale(0.001,0.001)")
         .selectAll("path")
         .data(dados.features)
         .enter()
         .append("path")
         .attr("class", "country")
         .attr("stroke", "#aaa")
-        .attr("stroke-width", "0.5")
-        .attr("fill", "transparent")
+        .attr("stroke-width", "20")
+        .attr("fill", "#000")
         .attr("d", pathGenerator);
     });
   }
